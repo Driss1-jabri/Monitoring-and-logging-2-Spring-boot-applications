6faaa2da79dae9b181fdbd636b0a1794009f566d
Driss1-jabri <jabri.dri.fst@uhp.ac.ma>
2024-08-18 12:12:08 +0100

    update app1 & app2

--- Spring boot applications/App1/src/main/java/org/example/app1/App1Application.java
+++ Spring boot applications/App1/src/main/java/org/example/app1/App1Application.java
@@ -1,6 +1,4 @@
 package org.example.app1;
-
-import co.elastic.apm.attach.ElasticApmAttacher;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -12,10 +10,7 @@ import java.util.Map;
 public class App1Application {
 
 	public static void main(String[] args) {
-		Map<String, String> configuration = new HashMap<>();
-		configuration.put("server_urls", "http://fleet-service:8200");
-		configuration.put("service_name", "App1");
-		ElasticApmAttacher.attach(configuration);
+
 		SpringApplication.run(App1Application.class, args);
 	}
 
