8088fbdbe5a2f8435a1a774e20b173f894e228c5
Driss1-jabri <jabri.dri.fst@uhp.ac.ma>
2024-08-21 16:08:58 +0100

    update app1

--- Spring boot applications/App1/src/main/java/org/example/app1/controller/App1Controller.java
+++ Spring boot applications/App1/src/main/java/org/example/app1/controller/App1Controller.java
@@ -1,18 +1,31 @@
 package org.example.app1.controller;
 
+import org.example.app1.model.Post;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RestController
 public class App1Controller {
+    private RestClient restClient;
 
     @GetMapping("/request")
     public String makeRequest() {
         RestTemplate restTemplate = new RestTemplate();
         String response = restTemplate.getForObject("http://app2-service:8080/data", String.class);
         return response;
+    }
+    @GetMapping("/posts")
+    public List<Post> allPosts(){
+        return restClient.get()
+                .uri("/posts")
+                .retrieve()
+                .body(new ParameterizedTypeReference<List<Post>>() {});
     }
 
 }
---
+++ Spring boot applications/App1/src/main/java/org/example/app1/model/Post.java
@@ -0,0 +1,4 @@
+package org.example.app1.model;
+
+public record Post(Integer id, Integer userId, String title, String body) {
+}
